239220c255fbfeaaf9f85a01e6733fa475ef294f
AignerZar <zarah.aigner@student.uibk.ac.at>
2026-03-25 12:48:04 +0100

    update
    Adjusted comments in vae_loss function to clarify beta and dimensions.

--- train/loss.py
+++ train/loss.py
@@ -13,9 +13,9 @@ import re
 from sklearn.model_selection import train_test_split
 
 # definition of the loss function    
-def vae_loss(x, x_hat, mu, logvar, beta=1.0):
-    x_hat = x_hat.view(x.size(0), 15, 9)
-    x = x.view(x.size(0), 15, 9)
+def vae_loss(x, x_hat, mu, logvar, beta=1.0):   # maybe adjusting beta further
+    x_hat = x_hat.view(x.size(0), 15, 9)     # 9 should be 6 for 2 atoms
+    x = x.view(x.size(0), 15, 9)             # 9 should be 6 for 2 atoms
     recon_loss = F.mse_loss(x_hat, x, reduction='sum') / x.size(0)
     kl_div = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) / x.size(0) #divinding for loss per batch
     return recon_loss + beta * kl_div, recon_loss, kl_div
