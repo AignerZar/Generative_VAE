30677da5a817380baada6ac1b6662a7dcb3e5bdc
AignerZar <zarah.aigner@student.uibk.ac.at>
2026-03-25 12:47:20 +0100

    update
    Updated the plot_loss function to improve visualization and changed the default output file name.

--- plotting/plot_loss.py
+++ plotting/plot_loss.py
@@ -15,14 +15,21 @@ from sklearn.model_selection import train_test_split
 """
 Plotting the training and validation loss course------------------------------------------------------------------------------------------------------------------------------------
 """
-def plot_loss(loss_history, outfile="loss.pdf"):
+def plot_loss(loss_history, outfile="loss_5000MC.pdf"):
     loss_history = np.array(loss_history)
-    plt.figure(figsize=(8,4))
-    plt.plot(loss_history[:,0], label=r"Train Loss")
-    plt.plot(loss_history[:,1], label=r"Val Loss")
-    plt.legend()
-    plt.xlabel(r"Epoch")
-    plt.ylabel(r"Loss")
+    plt.figure(figsize=(10,5))
+    plt.plot(loss_history[:,1], label=r"Validation Loss", linewidth=0.8)
+    plt.plot(loss_history[:,0], label=r"Training Loss", linewidth=0.8)
+    #plt.plot(loss_history[:,1], label=r"Val Loss")
+    plt.xlabel(r"Epoch", fontsize=14)
+    plt.ylabel(r"Loss", fontsize=14)
+
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    plt.legend(fontsize=13, loc="best", frameon=True)
+    #plt.xlabel(r"Epoch", fontsize=14)
+    #plt.ylabel(r"Loss", fontsize=12)
     plt.tight_layout()
     plt.savefig(outfile)
     plt.show()
